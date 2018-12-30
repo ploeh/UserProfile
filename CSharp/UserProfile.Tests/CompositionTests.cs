a8647fcd45ba1652b4c44cd8fb97807db0d288bd
Mark Seemann <mark@ploeh.dk>
2018-12-30 11:20:56 +0100

    Use monoidal composition followed by catamorphism

--- CSharp/UserProfile.Tests/CompositionTests.cs
+++ CSharp/UserProfile.Tests/CompositionTests.cs
@@ -14,17 +14,14 @@ namespace Ploeh.Samples.UserProfile.Tests
         {
             var repo = new FakeUserRepository();
             FillWithTestValues(repo);
-            IIconReader reader = new CompositeIconReader(
-                new GravatarReader(),
-                new IdenticonReader(),
-                new DBIconReader(repo));
+            var reader = new IconReaderFacade(repo);
 
             // The question corresponding to 42 is 'life, the universe, and
             // everything'. The Scandinavian first name Leif is pronounced like
             // the English 'life', and just so also happens to be the name of
             // my father :)
             var user = new User(42, "Leif Seemann", "leif@example.com", false, false);
-            Icon icon = reader.ReadIcon(user).GetValueOrDefault(Icon.Default);
+            Icon icon = reader.ReadIcon(user);
 
             var expected = new Icon(new Uri("https://example.com/users/42/icon"));
             Assert.Equal(expected, icon);
@@ -39,7 +36,7 @@ namespace Ploeh.Samples.UserProfile.Tests
             var sut = ComposeWithTracing(repo, observations);
 
             var user = new User(42, "Foo", "foo@example.com", true, true);
-            var actual = sut.ReadIcon(user).GetValueOrDefault(Icon.Default);
+            var actual = sut(user);
 
             var expected = new Icon(new Uri("https://www.gravatar.com/avatar/b48def645758b95537d4424c84d1a9ff"));
             Assert.Equal(expected, actual);
@@ -55,7 +52,7 @@ namespace Ploeh.Samples.UserProfile.Tests
             var sut = ComposeWithTracing(repo, observations);
 
             var user = new User(42, "Bar", "bar@example.org", false, true);
-            var actual = sut.ReadIcon(user).GetValueOrDefault(Icon.Default);
+            var actual = sut(user);
 
             var expected = new Icon(new Uri("https://example.com/identicon/baf7c704bc2907f2b43fdd462f850336"));
             Assert.Equal(expected, actual);
@@ -71,7 +68,7 @@ namespace Ploeh.Samples.UserProfile.Tests
             var sut = ComposeWithTracing(repo, observations);
 
             var user = new User(42, "Baz", "baz@example.net", false, false);
-            var actual = sut.ReadIcon(user).GetValueOrDefault(Icon.Default);
+            var actual = sut(user);
 
             var expected = new Icon(new Uri("https://example.com/users/42/icon"));
             Assert.Equal(expected, actual);
@@ -88,7 +85,7 @@ namespace Ploeh.Samples.UserProfile.Tests
 
             // 40 isn't in the 'database'
             var user = new User(40, "Qux", "qux@example.com", false, false);
-            var actual = sut.ReadIcon(user).GetValueOrDefault(Icon.Default);
+            var actual = sut(user);
 
             var expected = new Icon(new Uri("https://example.com/default-icon"));
             Assert.Equal(expected, actual);
@@ -102,13 +99,24 @@ namespace Ploeh.Samples.UserProfile.Tests
             repo.Add(666, "666");
         }
 
-        private IIconReader ComposeWithTracing(IUserRepository repository, ICollection<int> observations)
+        private Func<User, Icon> ComposeWithTracing(IUserRepository repository, ICollection<int> observations)
         {
-            return new CompositeIconReader(
+            var readers = new IIconReader[]
+            {
                 new TraceIconReader<int>(0, observations, new GravatarReader()),
                 new TraceIconReader<int>(1, observations, new IdenticonReader()),
                 new TraceIconReader<int>(2, observations, new DBIconReader(repository)),
-                new TraceIconReader<int>(3, observations, new NullIconReader()));
+                new TraceIconReader<int>(3, observations, new NullIconReader())
+            };
+            return u =>
+            {
+                var lazyIcons = readers
+                    .Select(r =>
+                        new Lazy<Maybe<First<Icon>>>(() =>
+                            r.ReadIcon(u).Select(i => new First<Icon>(i))));
+                var m = lazyIcons.FindFirst();
+                return m.Value.Aggregate(Icon.Default, fi => fi.Item);
+            };
         }
     }
 }
---
+++ CSharp/UserProfile.Tests/IconReaderFacade.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ploeh.Samples.UserProfile.Tests
+{
+    public class IconReaderFacade
+    {
+        private readonly IReadOnlyCollection<IIconReader> readers;
+
+        public IconReaderFacade(IUserRepository repository)
+        {
+            readers = new IIconReader[]
+                {
+                    new GravatarReader(),
+                    new IdenticonReader(),
+                    new DBIconReader(repository)
+                };
+        }
+
+        public Icon ReadIcon(User user)
+        {
+            IEnumerable<Lazy<Maybe<First<Icon>>>> lazyIcons = readers
+                .Select(r =>
+                    new Lazy<Maybe<First<Icon>>>(() =>
+                        r.ReadIcon(user).Select(i => new First<Icon>(i))));
+            Lazy<Maybe<First<Icon>>> m = lazyIcons.FindFirst();
+            return m.Value.Aggregate(Icon.Default, fi => fi.Item);
+        }
+    }
+}
